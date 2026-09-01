1626e9ed84a2b1bbc83f3b185cad874f5b1980bd
ctomris59-dev <ctomris59@gmail.com>
2026-09-01 10:19:35 +0300

    Type optional visual role label compatibility

---
+++ frontend/lib/hr/careerArchitecture.visual.d.ts
@@ -0,0 +1,8 @@
+import "./careerArchitecture";
+
+declare module "./careerArchitecture" {
+  interface CareerRole {
+    /** Optional presentation alias used only by visual summaries. */
+    levelName?: string;
+  }
+}
